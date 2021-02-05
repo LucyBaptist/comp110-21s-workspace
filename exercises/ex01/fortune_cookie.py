165cb147b7e28fa86e96ade1407bb72f1be03c68
Lucy Baptist <blythelu@emai.unc.edu>
2021-02-05 08:42:01 -0500

    progress ex01

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730386091"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,19 @@ from random import randint
 
 
 # Begin your solution here...
+fortune = randint(1,100)
+print("Your fortune cookie says...")
+if fortune < 60:
+    if fortune < 30:
+        print("You will find a pleasant surprise next week.")
+    else:
+        print("Someone will have good news for you soon.")
+if fortune < 85:
+    if fortune < 70:
+        print("Be on the watch for a new opportunity.")
+    else:
+        print("Expect to find a new friend in an unexpected place.")
+else:
+    print("Try your best, success is within your reach.")
+print("Now, go spread positive vibes!")
+
--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -1,6 +1,20 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730386091"
 
 
 # Begin your solution here...
+user_num = int(input("Please enter an int: "))
+
+if user_num % 2 == 0 and user_num % 7 == 0:
+    print("TAR HEELS")
+
+if user_num % 2 == 0 and user_num % 7 != 0:
+    print("TAR")
+
+if user_num % 7 == 0 and user_num % 2 != 0:
+    print("HEELS")
+
+if user_num % 7 != 0 and user_num % 2 != 0:
+    print("CAROLINA")
+    
