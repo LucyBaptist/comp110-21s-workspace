c51e610221c0ac73ec5f7af21f563e6fd2fe2c1e
Lucy Baptist <blythelu@emai.unc.edu>
2021-02-09 18:35:01 -0500

    progress

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,16 +1,23 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730386091"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heel(choice))
+    return None
 
-
-# TODO 1: Define the tar_heels function, and its logic, here.
-
+def tar_heel(choice: int) -> str:
+    if choice % 2 == 0 and choice % 7 == 0:
+        return("TAR HEELS")
+    if choice % 2 == 0 and choice % 7 != 0:
+        return("TAR")
+    if choice % 7 == 0 and choice % 2 != 0:
+        return("HEELS")
+    if choice % 7 != 0 and choice % 2 != 0:
+        return("CAROLINA")
 
 if __name__ == "__main__":
     main()
