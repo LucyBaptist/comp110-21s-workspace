816347d89114bd2219a2338d7d1cd589142dba88
Lucy Baptist <blythelu@emai.unc.edu>
2021-02-10 12:40:11 -0500

    progress

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -4,28 +4,24 @@ from random import randint
 
 __author__ = "730386091"
 
-fortune = randint(0,100)
+fortune = randint(0, 100)
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-
-    print(fortune_cookie(fortune))
+    print(fortune_cookie)
     print("Now, go spread positive vibes!")
     return None
 
-def fortune_cookie(fortune: int) -> str:
+def fortune_cookie(fortune) -> str:
     if fortune < 60:
         if fortune < 30:
             return("You will find a pleasant surprise next week.")
-        else:
-            return("Someone will have good news for you soon.")
+        return("Someone will have good news for you soon.")
     if fortune < 85:
         if fortune < 70:
             return("Be on the watch for a new opportunity.")
-        else:
-            return("Expect to find a new friend in an unexpected place.")
-    else:
-        return("Try your best, success is within your reach.")
+        return("Expect to find a new friend in an unexpected place.")
+    return("Try your best, success is within your reach.")
 # Python Idiom for "starting" the program when run as a module.
 # The special dunder variable __name__ will be "__main__" when run as module. 
 if __name__ == "__main__":
