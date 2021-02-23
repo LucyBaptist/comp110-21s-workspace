dd1140936c347e6918e790b2df4d6a003b0cfb1c
Lucy Baptist <blythelu@emai.unc.edu>
2021-02-23 16:52:41 -0500

    progress

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,19 +1,30 @@
 __author__ = "730386091"
 
-from typing import AnyStr
-
+from random import randint
 
 ANSWER_ONE: int = 1
 ANSWER_TWO: int = 30
 ANSWER_THREE: int = 100
 
-def main() -> None:
+while input("Would you liketo continue? yes/no: ") == "yes":
+    count: int = 0
+    def main() -> None:
     """Entry point."""
     print(greet())
+    result = (input("Type CONTINUE or RANDOM: "))
     print(question_1())
+    a_1: str = str(input("Please enter the letter of your answer here: "))
     print(question_2())
+    a_2: str = str(input("Please enter the letter of your answer here: "))
     print(question_3())
+    a_3: str = str(input("Please enter the letter of your answer here: "))
     print(point_keeping(a_1, a_2, a_3))
+    points = ans_1 + ans_2 + ans_3
+    count += 1
+    print(f"You have taken this quiz {count} times.")
+
+print(f"{player}, thank you for taking my quiz!")
+
 
 
 def greet() -> None:
@@ -21,43 +32,45 @@ def greet() -> None:
     print("Hello! This quiz will assign a sci-fi book series based on a series of questions that you wil answer.")
     print("Enter a name, then begin the quiz.")
     player: str = str(input("Please enter a name: "))
-    print(player + "Would you like to take the quiz or generate a random answer?")
-    result = (input("Type CONTINUE or RANDOM: "))
-    return result
+    print(f"{player}, would you like to take the quiz or generate a random answer?")
+    return None
 
 
-def question_1() -> str:
+def question_1() -> None:
     """Question 1."""
     print("Which of these emojis reflects a setting that you are interested?")
     print("a. \U0001F307 \n b. \U0001F304 \n c. \U0001F3DE ")
-    a_1 = str(input("Please enter the letter of your answer here: "))
-    return a_1
+    return None
 
 
-def question_2() -> str:
+def question_2() -> None:
     """Question 2."""
     print("Do you like aliens in fiction?")
     print("a. no \n b. yes, but only if they're basically humans \n c. yes, the more alien the better")
-    a_2 = str(input("Please enter the letter of your answer here: "))
-    return a_2
+    return None
 
 
-def question_3() -> str:
+def question_3() -> None:
     """Question 3."""
     print("How would you like the book serie sto make you feel?")
     print("a. \U0001F914 \n b. \U0001F92F \n c. \U0001F62E")
     a_3 = str(input("Please enter the letter of your answer here: "))
-    return a_3
+    return None
 
 
 def point_keeping(ans_1: str, ans_2: str, ans_3: str) -> int:
     """A way of keeping track of points to determine results."""
-    if ans_1 = "a":
+    if ans_1 == "a":
         then: ans_1 = ANSWER_ONE
-    if ans_1 = "b":
+    if ans_1 == "b":
         then: ans_1 = ANSWER_TWO
-    if ans_1 = "c":
+    if ans_1 == "c":
         then: ans_1 = ANSWER_THREE
+
+
+def answer(score: int) -> str:
+    """Gives the result of the quiz."""
+
 
 
 
