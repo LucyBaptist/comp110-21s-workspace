fe0469d7f39d02e8767c0ef40d019e75f31ee07e
Lucy Baptist <blythelu@emai.unc.edu>
2021-02-23 10:47:11 -0500

    progress

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,5 +1,8 @@
 __author__ = "730386091"
 
+from typing import AnyStr
+
+
 ANSWER_ONE: int = 1
 ANSWER_TWO: int = 30
 ANSWER_THREE: int = 100
@@ -10,6 +13,7 @@ def main() -> None:
     print(question_1())
     print(question_2())
     print(question_3())
+    print(point_keeping(a_1, a_2, a_3))
 
 
 def greet() -> None:
@@ -17,36 +21,43 @@ def greet() -> None:
     print("Hello! This quiz will assign a sci-fi book series based on a series of questions that you wil answer.")
     print("Enter a name, then begin the quiz.")
     player: str = str(input("Please enter a name: "))
-    return player
+    print(player + "Would you like to take the quiz or generate a random answer?")
+    result = (input("Type CONTINUE or RANDOM: "))
+    return result
 
-def question_1(a_1: str) -> str:
+
+def question_1() -> str:
     """Question 1."""
     print("Which of these emojis reflects a setting that you are interested?")
     print("a. \U0001F307 \n b. \U0001F304 \n c. \U0001F3DE ")
     a_1 = str(input("Please enter the letter of your answer here: "))
     return a_1
 
+
 def question_2() -> str:
     """Question 2."""
+    print("Do you like aliens in fiction?")
+    print("a. no \n b. yes, but only if they're basically humans \n c. yes, the more alien the better")
     a_2 = str(input("Please enter the letter of your answer here: "))
     return a_2
 
+
 def question_3() -> str:
     """Question 3."""
+    print("How would you like the book serie sto make you feel?")
+    print("a. \U0001F914 \n b. \U0001F92F \n c. \U0001F62E")
     a_3 = str(input("Please enter the letter of your answer here: "))
     return a_3
 
+
 def point_keeping(ans_1: str, ans_2: str, ans_3: str) -> int:
     """A way of keeping track of points to determine results."""
-    ans_1 = a_1
-    ans_2 = a_2
-    ans_3 = a_3
-    if ans_1 = a
-        then ans_1 = ANSWER_ONE
-    if ans_1 = b
-        then ans_1 = ANSWER_TWO
-    if ans_1 = c
-        then ans_1 = ANSWER_THREE
+    if ans_1 = "a":
+        then: ans_1 = ANSWER_ONE
+    if ans_1 = "b":
+        then: ans_1 = ANSWER_TWO
+    if ans_1 = "c":
+        then: ans_1 = ANSWER_THREE
 
 
 
