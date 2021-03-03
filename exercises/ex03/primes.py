2c0deb6e42b16df757b96f32d1da966388061368
Lucy Baptist <blythelu@emai.unc.edu>
2021-03-03 17:38:17 -0500

    progress

--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -5,6 +5,10 @@ __author__: str = "730386091"
 
 def main() -> None:
     """Entrypoint of the program."""
+    print(is_prime(3))
+    print(is_prime(6))
+    print(is_prime(31))
+    print(is_prime(110))
     # Put print statements here to test your function
     # ex. print(is_prime(5)), print(list_primes(10, 20))
 
@@ -13,13 +17,9 @@ def is_prime(test: int) -> bool:
     denom: int = 2
     while denom < test:
         if test % denom == 0:
-            ans: bool = True
-            return ans
-        else: 
-            ans: bool = False
-            return ans
+            return True
         denom += 1
-    return ans
+    return False
 
 
 if __name__ == "__main__":
