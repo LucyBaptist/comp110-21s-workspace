15cd3b9f059149e7630ae84f4fb841bc251035a6
Lucy Baptist <blythelu@emai.unc.edu>
2021-03-03 14:55:52 -0500

    p

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,12 +1,19 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730386091"
 
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(avoid_fifth("hello there"))
+
+
+def avoid_fifth(words: str) -> str:
+    """Removes e."""
+    while "e" in words == True:
+       words.strip("e")
+    return words
 
 
 if __name__ == "__main__":
--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,12 +1,14 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730386091"
 
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(palindromify("race", false))
+
+
 
 
 if __name__ == "__main__":
