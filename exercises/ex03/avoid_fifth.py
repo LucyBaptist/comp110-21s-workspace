c4bf7743d09e39043fca29687fe311a1348a6447
Lucy Baptist <blythelu@emai.unc.edu>
2021-03-03 16:20:46 -0500

    p

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -6,14 +6,17 @@ __author__: str = "730386091"
 def main() -> None:
     """Entrypoint of the program."""
     print(avoid_fifth("three"))
+    print(avoid_fifth("hello"))
+    print(avoid_fifth("The sentence we have here possesses E's galore!"))
 
 
 def avoid_fifth(words: str) -> str:
     """Removes e."""
     while "e" in words == True:
-       words = words.strip("e")
+       words.rstrip("e")
     return words
 
+       
 
 if __name__ == "__main__":
     main()
