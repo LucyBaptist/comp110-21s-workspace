b4213d3a7d10406bc55e13faf09d5c85ed7f4cd6
Lucy Baptist <blythelu@emai.unc.edu>
2021-03-04 09:10:46 -0500

    progress

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -5,11 +5,25 @@ __author__: str = "730386091"
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
+    print(palindromify("race", False))
+    print(palindromify("han", True))
+    print(palindromify("red", True))
+    print(palindromify("live on time ", False))
 
 
-
+def palindromify(word: str, tf: bool) -> str:
+    num: int = len(word)
+    pal: str = ""
+    if tf == True:
+        while num >= 0:
+            pal += word[num]
+            num -= 1
+    else:
+        while num >= 1:
+            pal += word[num - 1]
+            num -= 1 
+    return(word + pal)
+            
 
 if __name__ == "__main__":
     main()
