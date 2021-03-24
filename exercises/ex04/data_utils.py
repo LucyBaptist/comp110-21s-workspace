ef7db4ea037588bec7dedf7fc90ede6c66f532d9
Lucy Baptist <blythelu@emai.unc.edu>
2021-03-24 14:57:01 -0400

    progress

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -43,7 +43,7 @@ def head(table: dict[str, list[str]], row_num: int) -> dict[str, list[str]]:
         list_col = []
         i: int = 0
         while i < row_num:
-            list_col.append(table[column])
+            list_col.append(table[column[i]])
             i += 1
         dictionary[column] = list_col
     return dictionary
