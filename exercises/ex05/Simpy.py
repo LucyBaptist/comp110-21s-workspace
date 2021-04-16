eef5403fc6d14dc4c1558f34d2f33c39ac9c4443
Lucy Baptist <blythelu@emai.unc.edu>
2021-04-16 09:36:34 -0400

    progress

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -13,8 +13,10 @@ class Simpy:
     def __init__ (self, values: list[float]):
         self.values = values
     
+
     def __repr__ (self) -> str:
         return(f"{self.values}")
+
 
     def fill (self, val: float, length: int) -> None:
         i: int = 0
@@ -22,6 +24,7 @@ class Simpy:
             self.values.append(val)
             i += 1
         
+
     def arange (self, start: float, stop: float, step: float) -> None:
         assert step != 0.0
         self.values[0] = start
@@ -29,9 +32,50 @@ class Simpy:
         while i < stop:
             self.values.append(i)
             i *= step
-        
-        
     
+
     def sum (self) -> float:
         length: float = sum(self.values)
         return length
+
+
+    def __add__ (self, rhs: Union[float, Simpy]) -> Simpy:
+        result: list[float] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item + rhs)
+
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] + rhs.values[i])
+        
+        return Simpy(result)
+
+
+    def __pow___ (self, rhs: Union[float, Simpy]) -> Simpy:
+        result: list[float] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item ** rhs)
+
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] ** rhs.values[i])
+        
+        return Simpy(result)
+
+
+    def __mod__ (self, rhs: Union[float, Simpy]) -> Simpy:
+        result: list[float] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item % rhs)
+
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] % rhs.values[i])
+        
+        return Simpy(result)
